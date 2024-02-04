0a0a191f4b4688d35412ddfce3b13144a2117593
LZafiro <zafiro.unifesp@gmail.com>
2024-02-04 19:46:37 -0300

    feature(metadata): Added site title, desc and favicon

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: "TrelloClone",
+  description:
+    "Collaborate and get more done. TrelloClone's boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible, and rewarding way.",
+};
