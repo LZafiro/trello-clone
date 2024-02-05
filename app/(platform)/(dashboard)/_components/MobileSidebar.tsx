b5dc945f74de4f58745fe075d01f740645556b90
LZafiro <zafiro.unifesp@gmail.com>
2024-02-05 01:02:21 -0300

    feature(dashboard): sidebar impl with responsiveness

---
+++ app/(platform)/(dashboard)/_components/MobileSidebar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
+
+import { useMobileSidebar } from "@/hooks/use-mobile-sidebar";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+import Sidebar from "./Sidebar";
+
+export default function MobileSidebar() {
+  const pathName = usePathname();
+  const [isMounted, setIsMounted] = useState(false);
+
+  const isOpen = useMobileSidebar((state) => state.isOpen);
+  const onOpen = useMobileSidebar((state) => state.onOpen);
+  const onClose = useMobileSidebar((state) => state.onClose);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    onClose();
+  }, [pathName, onClose]);
+
+  // Workaround to some hydration issues
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Button
+        onClick={onOpen}
+        className="block md:hidden mr-2"
+        variant="ghost"
+        size="sm"
+      >
+        <Menu className="h-4 w-4" />
+      </Button>
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent side="left" className="p-2 pt-10">
+          <Sidebar storageKey="trello-clone-mobile-sidebar-state" />
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+}
