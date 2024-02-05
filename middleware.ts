2d0fa3969781d69ee2354a9cb9bb9d7b1e80701a
LZafiro <zafiro.unifesp@gmail.com>
2024-02-04 21:38:21 -0300

    feature(auth): Added after login logic to clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,34 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    // User is authenticated and is trying to access a public route
+    // Forces to redirect to the organization page. If the user has no organization,
+    // it will redirect to the select organization page
+    if (auth.userId && auth.isPublicRoute) {
+      let path;
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      } else {
+        path = "/select-org";
+      }
+      const url = new URL(path, req.url);
+      return NextResponse.redirect(url);
+    }
+    // If the user is not authenticated and is trying to access a private route
+    // redirect to the sign in page (this must be specified because "afterAuth"
+    // overrides the default behavior of the authMiddleware function)
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // If the user is authenticated and has no organization, redirect to the select organization page
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const url = new URL("/select-org", req.url);
+      return NextResponse.redirect(url);
+    }
+  },
 });
 
 export const config = {
